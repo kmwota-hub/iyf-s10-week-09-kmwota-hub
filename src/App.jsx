a61db2584651b595e375361d81a0b890455ac1ad
Mwotsam <kmwota@gmail.com>
2026-04-21 22:17:48 +0300

    Replace BrowserRouter with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import useLocalStorage from './hooks/useLocalStorage';
 import Home from './pages/Home';
 import Posts from './pages/Posts';
